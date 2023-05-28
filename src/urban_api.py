fbd6c2224591ae3a783647762d4134f76951a587
Joshua Rose <joshuarose099@gmail.com>
2023-05-28 12:54:29 +1000

    docs: add file docstrings
    * Add header docstrings
    
    * License apache
    
    * File description / file contents
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

---
+++ src/urban_api.py
@@ -0,0 +1,16 @@
+"""
+### Meta & Licensing
+__File__: `urban_utils.py`
+__License__: [Apache 2.0](https://gh-syn.github.io/urban-cli/license.html)
+__Author__: Joshua Rose <joshuarose099@gmail.com>
+
+### Module Description
+
+Contains centralized interactions with urban-cli.
+
+This module handles:
+ - sending requests
+ - processing responses
+ - handling API-related functionalities
+"""
+
---
+++ src/urban_utils.py
@@ -0,0 +1,15 @@
+"""
+### Meta & Licensing
+__File__: `urban_utils.py`
+__License__: [Apache 2.0](https://gh-syn.github.io/urban-cli/license.html)
+__Author__: Joshua Rose <joshuarose099@gmail.com>
+
+### Module Description
+
+Contains commonly used utility functions.
+
+This module handles:
+ - common data structures
+ - data manipulation
+ - io handling
+"""
