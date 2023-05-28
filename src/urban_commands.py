ecaec4b01c7042497d3735ad9f0a4e2a6bcca54f
Joshua Rose <joshuarose099@gmail.com>
2023-05-28 12:54:29 +1000

    docs: add file docstrings
    * Add header docstrings
    
    * License apache
    
    * File description / file contents
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

---
+++ src/urban_commands.py
@@ -0,0 +1,13 @@
+"""
+### Meta & Licensing
+__File__: `urban_commands.py`
+__License__: [Apache 2.0](https://gh-syn.github.io/urban-cli/license.html)
+__Author__: Joshua Rose <joshuarose099@gmail.com>
+
+### Module Description
+
+Contains individual command implementations for Urban CLI.
+
+This module handles individual command implementations.
+"""
+
