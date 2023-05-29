2189909eb83b4362cabd6ebd3eedcb0cf0263f21
Joshua Rose <joshuarose099@gmail.com>
2023-05-29 18:20:46 +1000

    implement: show error function, additional parser.
    * Error function that rich prints error doesn't exist
    
    * error function takes in word argument
    
    * Minimal spacing applied
    
    Issue: #55
    Tag: 2.0.0
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- src/urban.py
+++ src/urban.py
@@ -2,6 +2,8 @@
 Centralized urban file that is calls the API.
 """
 
+from urban_api import apply_word_to_url, send_exists_request, send_phrase_request
+
 
 def main():
     pass
--- src/urban_utils.py
+++ src/urban_utils.py
@@ -14,12 +14,12 @@ It is responsible for the following functions.
 Data Manipulation
 -----------------
 When a request object is received, it needs to be formatted in a way that Python can understand
-The beautifulsoup library helps beautifully ✨ with this. It parses html data in a way that can be 
+The beautifulsoup library helps beautifully ✨ with this. It parses html data in a way that can be
 read and manipulated very easily.
 
 I/O handling
 ------------
-The argparse library can be thought of as boilerplate level code in addition to a few extra classes 
+The argparse library can be thought of as boilerplate level code in addition to a few extra classes
 that greatly help building a command line interface. It removes most of the argument logic and lets the
 developer focus on what's important. Argparse is used in this file for showing and receiving data.
 
@@ -27,3 +27,33 @@ Common Data Structures
 -----------------------
 Common data structures such as a date, definition struct, lives in this file.
 """
+
+import argparse
+
+import rich
+
+
+PARSER = argparse.ArgumentParser(
+    prog="Urban",
+    description="Get any definition from Urban Dictionary",
+    # epilog="Licensed to Joshua Rose with Apache 2.0",
+)
+# NOTE: parser is a constant, thus is capitalised
+"""Parser object as `argparse.ArgumentParser` constant"""
+
+
+def show_does_not_exit_error(word: str):
+    """
+    Show does not exist error when invalid word is queried.
+
+    A type error is not raised as data type is checked in `send_phrase_request` function.
+
+    :param word: The invalid word that apparently doesn't exist.
+    """
+
+    rich.print("The word %s doesn't exist yet." % word, end=" ")
+    rich.print(
+        "You can change that by submitting a definition on the {link}".format(
+            link="[link url='https://urbandictionary.com/']Urban Dictionary website.[/link]"
+        )
+    )
