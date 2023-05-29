4fa3d1ef071b990841ffdf87319d812c9fa3d5aa
Joshua Rose <joshuarose099@gmail.com>
2023-05-29 19:07:17 +1000

    feat: invalid order class
    * Exception raised when order value is greater than found definitions
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- urban/urban_exceptions.py
+++ urban/urban_exceptions.py
@@ -36,3 +36,29 @@ class InvalidStatusCodeError(Exception):
         """
 
         return f"Invalid status code: {self.code}. Must be 200 or 404."
+
+class InvalidOrder(Exception):
+    """
+    Custom exception raised when an order is greater than the definitions found for a word.
+
+    :attr order: The invalid order number.
+    """
+
+    def __init__(self, order):
+        """
+        Initialize the InvalidOrder instance.
+
+        :param order: The invalid order number.
+        """
+
+        self.order = order
+
+    def __str__(self, definitions=0) -> str:
+        """
+        Return a string representation of the exception.
+
+        :param definitions: The amount of definitions that have been found.
+        :return: A string indicating the invalid order number and the amount of found orders.
+        """
+
+        return f"Invalid order: {self.order}. Found only {definitions} definitions."
