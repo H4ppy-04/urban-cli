51871ad1baf10afe043169c9af2850156944e09d
Joshua Rose <joshuarose099@gmail.com>
2023-05-30 16:07:42 +1000

    docs: add rework, merge about.rst and index.rst
    * Add rework page
    
    * merge about.rst and index.rst into one file
    
    * refactor 'contents' into 'table of contents'
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,10 @@ sys.path.insert(0, os.path.abspath("../.."))
 sys.path.insert(1, os.path.abspath("../../urban"))
 sys.path.insert(2, os.path.abspath("../../urban/"))
 
+import python_docs_theme
+import urban
+# from urban._urban import display_requests_error
+# from urban._urban import fetch_response_from_URL
 
 # html_theme = 'python_docs_theme'
 html_theme = "press"
