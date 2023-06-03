56fc503b89f1e4443b022a27654e8727cc453cd9
Joshua Rose <joshuarose099@gmail.com>
2023-06-03 10:35:18 +1000

    chore(pyright): ignore constructor file imports
    Ignore import statements that are unused in constructor.
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- urban/__init__.py
+++ urban/__init__.py
@@ -1,1 +1,1 @@
-from urban.urban_api import send_exists_request, send_phrase_request, apply_word_to_url
+from urban.urban_api import send_exists_request, send_phrase_request, apply_word_to_url  # pyright: ignore
