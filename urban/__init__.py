047b544c8a0834fbcbb2fd5d01777af6e94fd2ed
Joshua Rose <joshuarose099@gmail.com>
2023-06-03 11:24:49 +1000

    refactor: Remove dead code
    * remove dead code in the sphinx configuration file.
    
    * bump version from `0.1.0` to `1.0.2` in `version` variable
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- urban/__init__.py
+++ urban/__init__.py
@@ -1,1 +1,5 @@
-from urban.urban_api import apply_word_to_url, send_exists_request, send_phrase_request  # pyright: ignore
+from urban.urban_api import (
+    apply_word_to_url,
+    send_exists_request,
+    send_phrase_request,
+)  # pyright: ignore
