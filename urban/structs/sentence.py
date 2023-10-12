4bff7cc0dec8e15a5e07e8cc76d0c911c27eedcd
Joshua Rose <joshuarose099@gmail.com>
2023-10-13 08:21:30 +1100

    feat(structs): add structs file; modify documentation

---
+++ urban/structs/sentence.py
@@ -0,0 +1,19 @@
+"""
+File: sentence.py
+Author: Joshua Rose
+Email: joshuarose@gmx.com
+Github: https://github.com/JoshuaDRose
+Description: File container for sentence dataclass.
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Sentence:
+    """
+    This class holds the basic lexer properties
+    for a sentence.
+    """
+
+    pass
